cb2e5bc372f5195acd6d3653225ea8d5f2669382
MarkushaRaz <LegoAnimation2022@gmail.com>
2026-02-12 17:46:04 +0300

    1

---
+++ Server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+
+const port = process.env.PORT || 1000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    if (req.method === 'POST' && req.url === '/') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            const { text } = JSON.parse(body);
+
+            text.trim();
+
+            if (text.length > 50) {
+                res.writeHead(500, {'Content-Type': 'application/json'});
+                res.end();
+                return;
+            }
+            else {
+                const resultPath = path.join(__dirname, 'result.wav');
+
+                if (fs.existsSync(resultPath)) {
+                    fs.unlinkSync(resultPath);
+                }
+
+                const command = `tts --model_name tts_models/multilingual/multi-dataset/xtts_v2 --text "${text}" --speaker_wav Main.wav --language_idx ru --out_path result.wav`;
+
+                exec(command, { cwd: __dirname }, async (error, stdout, stderr) => {
+                    res.writeHead(200);
+                    res.end(resultPath);
+
+                    fs.unlinkSync(resultPath);
+                });
+                return;
+            }
+
+            return;
+        });
+        return;
+    }
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log('> Successful start');
+});
